44e29d7e5894de163ab5787003aeabdc65399597
hippieZhou <hippiezhou@outlook.com>
2022-03-30 21:40:37 +0800

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <a className="App-link" href="/">
-          hippiezhou.dev
+          wwww.hippiezhou.dev
         </a>
       </header>
     </div>
