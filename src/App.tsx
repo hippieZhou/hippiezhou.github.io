fbc3dd4dae048c455b11c22e8a84e6cf92b6e455
hippieZhou <hippiezhou@outlook.com>
2022-04-01 10:38:21 +0800

    change layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,8 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  // const navItems = ["Home", "Posts", "Projects", "Tools", "About"];
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a className="App-link" href="/">
-          hippiezhou.dev
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
