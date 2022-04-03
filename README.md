85960f8638f3fe0ed653ddff810e4769215340e9
hippieZhou <hippiezhou@outlook.com>
2022-04-03 18:22:39 +0800

    use yarn

--- README.md
+++ README.md
@@ -9,5 +9,6 @@
 - [blog.evanyou.me/](https://blog.evanyou.me)
 - [evanyou.me/](https://evanyou.me)
 - [BearJS/yt-react-gh](https://github.com/BearJS/yt-react-gh)
-- [rafgraph/spa-github](https://github.com/rafgraph/spa-github-pages/blob/gh-pages/src/components/PageNotFound.tsx)
+- [rafgraph/spa-github-pages](https://github.com/rafgraph/spa-github-pages)
 - [deploy-react-app-with-react-router-to-github-pages-for-free](https://medium.com/@arijit_chowdhury/deploy-react-app-with-react-router-to-github-pages-for-free-569377f483f)
+- [Deploy React Apps with React Router to Github Pages](https://www.youtube.com/watch?v=1wDzEjXbblM)
--- src/Router.js
+++ src/Router.js
@@ -1,17 +1,17 @@
 import React from "react";
-import { Route, BrowserRouter, Switch } from "react-router-dom";
+import { HashRouter, Route, Routes, Switch } from "react-router-dom";
 import App from "./pages/App";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 
 const Router = () => (
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <Switch>
-      <Route path="/" exact component={App} />
-      <Route path="/about" exact component={About} />
-      <Route element={NotFound} />
-    </Switch>
-  </BrowserRouter>
+  <HashRouter basename={process.env.PUBLIC_URL}>
+    <Routes>
+      <Route path="/" exact element={<App></App>} />
+      <Route path="/about" exact element={<About></About>} />
+      <Route element={<NotFound></NotFound>} />
+    </Routes>
+  </HashRouter>
 );
 
 export default Router;
