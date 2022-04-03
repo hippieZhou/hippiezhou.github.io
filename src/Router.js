11a34665cfcec277eeeb08c750c857a9f67d1a52
hippieZhou <hippiezhou@outlook.com>
2022-04-03 17:01:20 +0800

    update react-router-dom version to 5.3.0

--- src/Router.js
+++ src/Router.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Route, BrowserRouter, Switch } from "react-router-dom";
 import App from "./pages/App";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 
 const Router = () => (
   <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <Routes>
-      <Route path="/" exact element={<App />} />
-      <Route path="/about" element={<About />} />
-      <Route element={<NotFound />} />
-    </Routes>
+    <Switch>
+      <Route path="/" exact component={App} />
+      <Route path="/about" component={About} />
+      <Route element={NotFound} />
+    </Switch>
   </BrowserRouter>
 );
 
