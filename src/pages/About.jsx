ce3c379bf609ab3d3f45c22f3c97f41acb825191
hippieZhou <hippiezhou@outlook.com>
2022-04-03 19:08:26 +0800

    add about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,25 @@
 import Layout from "../Layout";
 
 const About = () => {
-  return <Layout>About</Layout>;
+  return (
+    <Layout>
+      <img
+        style={{
+          height: "auto",
+        }}
+        alt="hippieZhou"
+        width="260"
+        height="260"
+        class="avatar avatar-user width-full border color-bg-default"
+        src="https://avatars.githubusercontent.com/u/13598361?v=4"
+      ></img>
+      <br />
+      <code>
+        hi, my name is QiangZhou, you can call me @hippieZhou. I'm a .NET
+        developer and currently work at @Thoughtworks.
+      </code>
+    </Layout>
+  );
 };
 
 export default About;
